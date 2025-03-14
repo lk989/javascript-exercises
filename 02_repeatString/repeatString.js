4931232e32d8ba61867b9bb88f5876b441c11918
lk989 <lama.bugis@hotmail.com>
2025-03-14 03:10:23 +0300

    solve problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, repetition) {
+    if(repetition < 0)
+        return "ERROR";
+    
+    let newString = "";
+    
+    for(i = 0; i < repetition; i++)
+    {
+        newString += text;
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
